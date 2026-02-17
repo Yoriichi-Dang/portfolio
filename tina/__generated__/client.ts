1a5217736ca206cb028dca17700d6d69b1abffe8
Nguyen Dang <nguyen.dang@c0x12c.com>
2026-02-17 11:33:44 +0700

    test

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: '/Users/nguyendang/Documents/workspace/dev/personal/nguyen_portfolio/nguyendang-portfolio/tina/__generated__/.cache/1771302134752', url: 'https://content.tinajs.io/2.1/content/59dace28-4c42-4aa8-b07c-d7f053ac0363/github/develop', token: '121820824c75191335950e45b196668861001f94', queries,  });
+export const client = createClient({ cacheDir: '/Users/nguyendang/Documents/workspace/dev/personal/nguyen_portfolio/nguyendang-portfolio/tina/__generated__/.cache/1771302341668', url: 'https://content.tinajs.io/2.1/content/59dace28-4c42-4aa8-b07c-d7f053ac0363/github/develop', token: '121820824c75191335950e45b196668861001f94', queries,  });
 export default client;
   
